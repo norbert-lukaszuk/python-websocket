9e7cb37d0c9d75aa265fd76c8b1681dfc7607182
norbert-lukaszuk <norbert.lukaszuk@gmail.com>
2023-02-18 11:12:47 +0100

    add some styles

--- show_time.js
+++ show_time.js
@@ -1,10 +1,15 @@
 window.addEventListener("DOMContentLoaded", () => {
   const messages = document.createElement("ul");
+  const clockDiv = document.getElementById("clock");
+  const clock = document.createElement("h1");
+  clock.setAttribute("class", "clock");
+  clockDiv.appendChild(clock);
   document.body.appendChild(messages);
 
   const websocket = new WebSocket("ws://localhost:5678/");
 
   websocket.onmessage = ({ data }) => {
+    clock.innerText = dayjs(data).format("hh:mm:ss");
     const message = document.createElement("li");
     const content = document.createTextNode(
       dayjs(data).format("hh:mm:ss__DD-MM-YYYY")
