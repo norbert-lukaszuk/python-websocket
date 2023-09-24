ec0878a66f7e0c5f97de11986f6f463e1bd616fe
norbert-lukaszuk <norbert.lukaszuk@google.com>
2023-09-24 14:24:25 +0200

    add notification back py file but still no cron task

--- show_time.py
+++ show_time.py
@@ -5,7 +5,7 @@ import datetime
 import json
 import websockets
 import Adafruit_DHT
-
+import requests
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
@@ -16,12 +16,18 @@ async def show_time(websocket):
         if(temperature!=None and humidity!=None):
             temp = "{:.2f}".format(temperature)
             humid = "{:.2f}".format(humidity)
-            
-        # humid = "{:.1f}".format(humidity)
             await websocket.send(json.dumps({"time":time,"temp":temp,"humid":humid}))
         await asyncio.sleep(1)
 
 async def main():
+    requests.post("https://ntfy.sh/my_posts", 
+        data="Websocket server is running!", 
+        headers={
+        "Title":"Raspberry pi logs",
+        "Priority":"default",
+        "Tags":"computer, wrench, +1"
+        })
+    
     async with websockets.serve(show_time, "", 5678):
         await asyncio.Future()  # run forever
 
